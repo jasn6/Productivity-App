e08a9821c22d78f1333789689f3b5b600c12f037
jasn6 <110363877+jasn6@users.noreply.github.com>
2023-01-16 22:38:29 -0500

    landing

---
+++ mystudyapp/frontend/src/components/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
+}
