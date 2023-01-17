d97bd363bdec773c7b9963ad5947717222151913
jasn6 <110363877+jasn6@users.noreply.github.com>
2023-01-17 12:02:22 -0500

    Material-UI
    NavrBar

--- mystudyapp/frontend/src/components/App.js
+++ mystudyapp/frontend/src/components/App.js
@@ -1,10 +1,17 @@
+import { Grid } from "@mui/material";
 import React, { Component } from "react";
 import { render } from "react-dom";
+import Header from "./Header";
 
 export default function App() {
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <Grid container spacing={10}>
+      <Grid item xs={12}>
+        <Header />
+      </Grid>
+      <Grid item xs={12}>
+        <div>HOME PAGE</div>
+      </Grid>
+    </Grid>
   );
 }
---
+++ mystudyapp/frontend/src/components/Header.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { createMuiTheme, ThemeProvider } from "@mui/material";
+import styled from "@mui/styled-engine-sc";
+import { AppBar, Toolbar, Typography, Button, IconButton } from "@mui/material";
+import { Menu as MenuIcon } from "@mui/icons-material";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#FFFAF0",
+    },
+    secondary: {
+      main: "#f50057",
+    },
+  },
+});
+
+const NavbarWrapper = styled("div")`
+  background-color: #fafafa;
+  box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
+    0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
+`;
+const NavbarTitle = styled(Typography)`
+  flex-grow: 1;
+`;
+
+const Navbar = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <NavbarWrapper>
+        <AppBar>
+          <Toolbar>
+            <IconButton edge="start" color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <NavbarTitle variant="h6">MyStudyApp</NavbarTitle>
+            <Button sx={{ color: "black" }}>Register</Button>
+            <Button sx={{ color: "black" }}>Login</Button>
+          </Toolbar>
+        </AppBar>
+      </NavbarWrapper>
+    </ThemeProvider>
+  );
+};
+
+export default Navbar;
