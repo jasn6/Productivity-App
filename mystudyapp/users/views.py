258a4eb63ffc57747f7b894cb358c71fedac66b0
jasn6 <110363877+jasn6@users.noreply.github.com>
2023-01-18 10:14:10 -0500

    Create User view + serializer
    Tested with postman.

--- mystudyapp/users/views.py
+++ mystudyapp/users/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import CustomUserSerializer
+from rest_framework.permissions import AllowAny
+from .models import NewUser
 
-# Create your views here.
+
+class CustomUserCreate(APIView):
+    permission_classes = [AllowAny]
+    serializer_class = CustomUserSerializer
+
+    def post(self, request, format=None):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data.get("user_name")
+            queryset = NewUser.objects.filter(user_name=user)
+            if queryset.exists():
+              return Response({'Error': 'Username Already Exists'}, status=status.HTTP_400_BAD_REQUEST)
+            else:
+                serializer.save()
+                json = serializer.data
+                return Response(json, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
