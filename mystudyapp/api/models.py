3c9512fa2be034abcc46c9a304d453b5dd68060c
jasn6 <110363877+jasn6@users.noreply.github.com>
2023-01-25 18:15:27 -0500

    Room name field
    Rooms should have a name.

--- mystudyapp/api/models.py
+++ mystudyapp/api/models.py
@@ -2,7 +2,8 @@ from django.db import models
 import string
 import random
 from django.conf import settings
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 def generate_unique_code():
     length = 6
@@ -22,3 +23,5 @@ class Room(models.Model):
     capacity = models.IntegerField(default=1)
     theme = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
+    
+    name = models.CharField(max_length=200, default='Room')
