74f515956424de1d984cab25d9092da7a816514d
jasn6 <110363877+jasn6@users.noreply.github.com>
2023-01-26 20:19:52 -0500

    Install Pexels
    Obtain backgrounds from pexels api.

--- mystudyapp/frontend/src/components/Room.js
+++ mystudyapp/frontend/src/components/Room.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { Button, Grid, Typography } from "@mui/material";
+import { createClient } from "pexels";
 
 export default function Room(props) {
   const navigate = useNavigate();
@@ -9,6 +10,12 @@ export default function Room(props) {
 
   React.useEffect(() => {
     getRoomDetails();
+    const client = createClient(
+      "ZDTi3aaQbzp3ZPwbSmpMnyFQtgQO70IUKxhxBxtQbmEnubRDNOT7cXO3"
+    );
+    client.collections.media({ per_page: 1, id: "u2qrglb" }).then((media) => {
+      console.log(media);
+    });
   }, []);
 
   const getRoomDetails = () => {
@@ -23,28 +30,36 @@ export default function Room(props) {
   };
 
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={12} align="center">
-        <Typography variant="h4">Code: {roomCode}</Typography>
+    <>
+      <Grid container spacing={2}>
+        <Grid item xs={12} align="center">
+          <Typography variant="h4">Code: {roomCode}</Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Typography variant="h6">Theme: {data.theme}</Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Typography variant="h6">
+            RoomName: {data.name != null ? data.name : ""}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Typography variant="h6">
+            Capacity {data.capacity != null ? data.capacity : ""}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Button color="secondary" variant="contained" href="/">
+            Leave Room
+          </Button>
+        </Grid>
       </Grid>
-      <Grid item xs={12} align="center">
-        <Typography variant="h6">Theme: {data.theme}</Typography>
-      </Grid>
-      <Grid item xs={12} align="center">
-        <Typography variant="h6">
-          RoomName: {data.name != null ? data.name : ""}
-        </Typography>
-      </Grid>
-      <Grid item xs={12} align="center">
-        <Typography variant="h6">
-          Capacity {data.capacity != null ? data.capacity : ""}
-        </Typography>
-      </Grid>
-      <Grid item xs={12} align="center">
-        <Button color="secondary" variant="contained" href="/">
-          Leave Room
-        </Button>
-      </Grid>
-    </Grid>
+      <video autoPlay loop muted playsInline className="room-back">
+        <source
+          src="https://player.vimeo.com/external/269971860.hd.mp4?s=eae965838585cc8342bb5d5253d06a52b2415570&profile_id=174&oauth2_token_id=57447761"
+          type="video/mp4"
+        />
+      </video>
+    </>
   );
 }
